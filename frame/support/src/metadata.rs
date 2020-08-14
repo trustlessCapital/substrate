17a22db96c784a44786385c18b8e040beccf9dac
Andrew Jones <ascjones@gmail.com>
2020-08-14 08:45:44 +0100

    Call tests passing

--- frame/support/src/metadata.rs
+++ frame/support/src/metadata.rs
@@ -567,7 +567,7 @@ mod tests {
 						vnext::FunctionMetadata {
 								name: "aux_0",
 								arguments: Vec::new(),
-								documentation: Vec::new(),
+								documentation: vec![" Doc comment put in metadata"],
 							}]),
 					// event: Some(DecodeDifferent::Encode(
 					// 	FnEncode(||&[
@@ -618,7 +618,7 @@ mod tests {
 					// 		)
 					// 	}),
 					// )),
-					calls: None,
+					calls: Some(vec![]),
 					// event: Some(DecodeDifferent::Encode(
 					// 	FnEncode(||&[
 					// 		EventMetadata {
